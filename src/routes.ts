9e5fe503708da0b0de1222fb6d5407850a40c2a1
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2021-11-06 12:40:15 -0300

    test ping pong v2 and test heathcheck

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const routes = express.Router();
 
 const studentsController = new StudentsController();
 
-routes.get("/ping", (_, res) => res.json("pong"));
+routes.get("/ping", (_, res) => res.json("pongv2"));
 
 routes.get("/students", studentsController.get);
 routes.post(
